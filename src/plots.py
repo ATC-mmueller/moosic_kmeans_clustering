36689f51f5009e0ac42f1da850fcb45a0ca970ff
Marcus <mueller.marcus.go@gmail.com>
2022-06-22 15:42:28 +0200

    added src-folder containing a function, created main-file

---
+++ src/plots.py
@@ -0,0 +1,12 @@
+def subplot_minmax(df):
+    from matplotlib import pyplot as plt
+    df_clusters = []
+    fig, axs = plt.subplots(9, 4, figsize=(14, 24), sharex='row')
+    for i in range(4):
+        df_clusters.append(df[df.cl_minmax == i])
+        axs[0, i].set_title(f'Cluster {i}')
+    for j in range(9):
+        axs[j,0].set_ylabel(df.columns[j])
+        for i in range(4):
+            axs[j, i].hist(df_clusters[i][df.columns[j]], bins=50)
+    plt.show()
