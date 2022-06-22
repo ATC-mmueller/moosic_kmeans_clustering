6effe473390e83bb218783b31505a8872186349f
Marcus <mueller.marcus.go@gmail.com>
2022-06-22 16:11:02 +0200

    added coloring to function

--- src/plots.py
+++ src/plots.py
@@ -8,5 +8,27 @@ def subplot_minmax(df):
     for j in range(9):
         axs[j,0].set_ylabel(df.columns[j])
         for i in range(4):
-            axs[j, i].hist(df_clusters[i][df.columns[j]], bins=50)
+            axs[j, i].hist(df_clusters[i][df.columns[j]], bins=50, color = colortab(j))
     plt.show()
+    
+def colortab(n):
+    if n == 0:
+        return 'tab:blue'
+    if n == 1:
+        return 'tab:orange'
+    if n == 2:
+        return 'tab:green'
+    if n == 3:
+        return 'tab:red'
+    if n == 4:
+        return 'tab:purple'
+    if n == 5:
+        return 'tab:brown'
+    if n == 6:
+        return 'tab:pink'
+    if n == 7:
+        return 'tab:gray'
+    if n == 8:
+        return 'tab:olive'
+    if n == 9:
+        return 'tab:cyan'
